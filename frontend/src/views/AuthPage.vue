f84e298ee923782b2fc4438315c1a5608ae0f19f
Java0range <javared234@gmail.com>
2025-05-09 11:17:16 +0300

    Add: User Logic

--- frontend/src/views/AuthPage.vue
+++ frontend/src/views/AuthPage.vue
@@ -11,9 +11,29 @@ const loginWindowFlag = ref<boolean>(true);
 const registerUsername = ref<string>("");
 const registerPassword = ref<string>("");
 
+const loginUsername = ref<string>("");
+const loginPassword = ref<string>("");
+
 const userStore = useUserStore();
 
 const router = useRouter();
+
+const loginUser = async () => {
+  try {
+    const data = await AuthService.login(loginUsername.value, loginPassword.value);
+    if (data.status === 200) {
+      userStore.userInfo = data.data;
+      await router.push({ path: "/me" });
+    }
+  } catch (err: any) {
+    if (err.response.status === 406) {
+      errorTitle.value = err.response.data;
+      errorFlag.value = true;
+    } else {
+      console.log(err);
+    }
+  }
+}
 
 const registerUser = async () => {
   try {
@@ -52,14 +72,14 @@ const registerUser = async () => {
       <p class="text-red-800">{{ errorTitle }}</p>
     </div>
     <div class="relative z-0 w-3/4 mb-5 group">
-      <input type="text" name="floating_first_name" class="w-full block py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-sky-500 peer" placeholder=" " required />
+      <input v-model="loginUsername" type="text" name="floating_first_name" class="w-full block py-2.5 px-0 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-sky-500 peer" placeholder=" " required />
       <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-sky-500 peer-focus:dark:text-sky-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Логин</label>
     </div>
     <div class="relative z-0 w-3/4 mb-8 group">
-      <input type="password" name="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-sky-500 peer" placeholder=" " required />
+      <input v-model="loginPassword" type="password" name="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-sky-500 peer" placeholder=" " required />
       <label for="floating_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-sky-500 peer-focus:dark:text-sky-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Пароль</label>
     </div>
-    <button class="bg-sky-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded-full transition-all">
+    <button @click="loginUser" class="bg-sky-500 hover:bg-sky-400 text-white font-bold py-2 px-4 rounded-full transition-all">
       Вход
     </button>
     <p class="mt-3">Нет аккаунта? <button @click="loginWindowFlag = !loginWindowFlag" class="text-sky-500 hover:text-sky-300 transition">Зарегистрируйся</button></p>
--- frontend/src/views/UserAccountPage.vue
+++ frontend/src/views/UserAccountPage.vue
@@ -1,7 +1,46 @@
 <script setup lang="ts">
+import { useUserStore } from '@/stores/user.ts'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+
+const username = ref<string>("");
+
+const router = useRouter();
+
+onMounted(() => {
+  console.log(userStore.userInfo)
+  username.value = userStore.userInfo.username;
+})
 
 </script>
 
 <template>
+  <div class="bg-white w-3/5 m-auto rounded-xl shadow-[0_0_25px_rgb(255,255,255)] shadow-sky-500 mt-20">
+    <header class="flex justify-between border-b border-slate-200 px-10 py-8">
+      <div class="flex items-center gap-4">
+        <h2 class="font-bold text-xl">{{ username }}</h2>
+      </div>
 
+      <ul class="flex items-center gap-10">
+        <li @click="router.push({ path: '/' })" class="flex items-center gap-3 cursor-pointer hover:-translate-y-1 transition duration-200">
+          <span>На главную</span>
+        </li>
+        <li class="flex items-center gap-1 cursor-pointer hover:-translate-y-1 transition duration-200">
+          <span>Избранное</span>
+          <img src="/yellow_star.svg" alt="star" class="h-5">
+        </li>
+        <li class="flex items-center gap-1 cursor-pointer hover:-translate-y-1 transition duration-200">
+          <span>Выйти</span>
+          <img src="/logout.svg" alt="logout" class="h-5">
+        </li>
+      </ul>
+    </header>
+    <div class="p-10">
+      <div class="flex justify-between items-center mb-10">
+        <h1 class="text-3xl font-bold">Резюме:</h1>
+      </div>
+    </div>
+  </div>
 </template>
