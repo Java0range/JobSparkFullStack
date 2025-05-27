8e551854c842d6118aae191947af9857c0387f0b
Java0range <javared234@gmail.com>
2025-05-27 21:51:24 +0300

    Delete: test.py

--- backend/src/test.py
+++
@@ -1,9 +0,0 @@
-from src.resumes.orm import ResumeORM
-import asyncio
-
-
-async def main():
-    print(await ResumeORM.get_resumes_for_filter(words_list=["python", "fastapi"]))
-
-
-asyncio.run(main())
